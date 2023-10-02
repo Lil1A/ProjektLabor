03ef4d405c2defc65355bbba92d57a18c553f5e0
Duba Bence <duba.bence22@gmail.com>
2023-10-02 08:31:26 +0200

    first

--- src/RegisterPage/RegisterPage.js
+++ src/RegisterPage/RegisterPage.js
@@ -6,9 +6,12 @@ function RegisterPage()
     return(
         <div className='RegisterPageWrapper'>
             <div className='RegisterPageInputHolder'>
-                Nincs még fiókod? Regisztrálj<br></br>
-              <input className='InputHolder' placeholder='Email cím' id='email'></input><br></br>
-              <input className='InputHolder' placeholder='Jelszó' id='password'></input>
+                <h1>Belépés</h1><br></br>
+                <input placeholder='Vezetéknév' className='InputHolder2'></input>
+            <input placeholder='Keresztnév' className='InputHolder2'></input>
+              <input type='email' className='InputHolder' placeholder='Email cím' id='email'></input><br></br>
+              <input type='password' className='InputHolder' placeholder='Jelszó' id='password'></input><br></br>
+               <button type='submit'>Belépés</button>            
             </div>
         </div>
     );
